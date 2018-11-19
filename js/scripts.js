f0c99c8fbee420e4cd251dd62ea6a396b9ef68b0
“SivonneWard” <sivonne@live.unc.edu>
2018-11-18 23:05:16 -0500

    changed the function so that the ajax call to get from space station was not inside another function, and the console log actually displayed the coordinates of the ISS

--- js/scripts.js
+++ js/scripts.js
@@ -2,47 +2,43 @@ $(document).ready(function(){
   console.log('scripts loaded');
 
   //declare variables
-  var url = 'http://api.open-notify.org/iss-now.json';
-  var url2 = 'https://nominatim.openstreetmap.org/reverse';
+  // var url = 'http://api.open-notify.org/iss-now.json';
+  // var url2 = 'https://nominatim.openstreetmap.org/reverse';
   var data = [];
-  var coos = [];
+  var html = '';
   //function to get the coordinates of the space station
-  function getLongLat(){
     $.ajax({
       type:'GET',
-      url:url,
+      url:'http://api.open-notify.org/iss-now.json',
       dataType:'json',
       async:true,
       data:data,
       success:function(data){
-        var lat = iss_position.latitude;
-        var long = iss_position.longitude;
-        var coordinates = [lat, long];
-        var html = '';
-
+        var lat = data['iss_position']['latitude'];
+        var lon = data['iss_position']['longitude'];
+        var coordinates = [lat, lon];
+        console.log(coordinates);
         //internal AJAX call to translate the lat and long to name of place
-        function coordinateToName(){
-          $.ajax({
-            type:'GET',
-            url:url2,
-            dataType:'json',
-            async:true,
-            data:coordinates,
-            success:function(coordinates){
-              //putting in the html
-              html += '<div class="location">';
-              html += '<h2>Location of the International Space Station Right Now</h2>';
-              html += '<p>The space station is currently over </p>' + getLongLat().coordinates + '<p> </p>';
-              html += '</div>';
-            }//closes second success function
-          });//closes second AJAX call
-          $('#results').html(html);
-
-        }
+            function coordinateToName(){
+              $.ajax({
+                type:'GET',
+                url:'https://nominatim.openstreetmap.org/reverse',
+                dataType:'json',
+                async:true,
+                data:coordinates,
+                success:function(coordinates){
+                  //putting in the html
+                  html += '<div class="location">';
+                  html += '<h2>Location of the International Space Station Right Now</h2>';
+                  html += '<p>The space station is currently over </p>' + getLongLat().coordinates + '<p> </p>';
+                  html += '</div>';
+                }//closes second success function
+              });//closes second AJAX call
+              $('#results').html(html);
+            }
       }//closes first success function
     });
     setInterval(5000);
-  }
 
 
 
